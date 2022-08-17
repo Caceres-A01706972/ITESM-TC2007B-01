0417afe00310167640bd12395a3c6ddc344c2582
Caceres-A01706972 <A01706972@tec.mx>
2022-08-16 19:51:44 -0500

    Initial Kotlin baseLine

---
+++ app/src/main/java/com/example/itesm_tc2007b_01/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.itesm_tc2007b_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var myTxt : String
+
+        //Asignamos variables a los elementos que pusinmos en el XML
+        var myTextView =  findViewById<TextView>(R.id.textView)
+        var myEditText = findViewById<EditText>(R.id.editTxt)
+        var myButton = findViewById<Button>(R.id.button)
+        
+        //Cuando le de click al boton
+        myButton.setOnClickListener{
+            myTxt = myEditText.text.toString()
+            myTextView.text = myTxt
+        }
+
+    }
+}
